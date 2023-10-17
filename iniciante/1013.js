3ffe02d61db428043efc8b6e6b24e14987f881ae
Renan Pereira de Sousa <99266774+RenanSousa26@users.noreply.github.com>
2023-10-17 18:25:39 -0300

    Delete iniciante/1013.js

--- iniciante/1013.js
+++
@@ -1,4 +0,0 @@
-var input = require('fs').readFileSync('../stdin', 'utf8');
-const A = input.split(" ").reduce((acum, item) => acum > item ? Number(acum) : Number(item), 0);
-
-console.log(`${A} eh o maior`);
