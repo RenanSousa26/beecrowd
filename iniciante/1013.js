a87d27ca4cbcad18b431c34181d1709cbe164a2a
silvez4 <gabriel.ieda@hotmail.com>
2023-10-17 17:36:33 -0300

    1013.js

---
+++ iniciante/1013.js
@@ -0,0 +1,4 @@
+var input = require('fs').readFileSync('../stdin', 'utf8');
+const A = input.split(" ").reduce((acum, item) => acum > item ? Number(acum) : Number(item), 0);
+
+console.log(`${A} eh o maior`);
